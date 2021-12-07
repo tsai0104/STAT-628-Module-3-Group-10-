0596058905e18eec9012221c6bbf8a4a1c41fc3a
Lanxin-Xiang <91483229+Lanxin-Xiang@users.noreply.github.com>
2021-12-07 13:28:52 -0600

    Update aspect.py

--- Aspect/aspect.py
+++ Aspect/aspect.py
@@ -68,10 +68,9 @@ def get_cur_aspect_adj(text):
 
 
 def get_segment(review_text, Aspects, default=''):
+    review_text = review_text.lower()
     res = []
-
-    # if is_review_only_one_aspect(review_text):
-    #     res.append(review_text)
+    c_score = []
 
     def get_all(review_text, aspect, default):
         if aspect not in review_text:
@@ -118,6 +117,12 @@ def get_segment(review_text, Aspects, default=''):
                 cur_aspect_end_index_end -= len(relation_store)
                 break
 
+                # 在下一个aspect截取
+                # if next_aspect != None:
+                # 	if next_aspect in aspects_dic and next_aspect in cur_strs:
+                # 		cur_aspect_end_index_end -= len(next_aspect)
+                # 		break
+
                 # 在aspect最近的形容词截取
             if cur_aspect_des is not None:
                 if cur_aspect_des in cur_strs:
@@ -133,63 +138,107 @@ def get_segment(review_text, Aspects, default=''):
             sentence = sentence.replace(item, '')
 
         if aspect in sentence and aim not in res:
-            res.append(aim)
+            text_tokens = word_tokenize(aim)
+            tokens_without_sw = [word for word in text_tokens if word not in stopwords]
+            aim = (" ").join(tokens_without_sw)
+            vs = analyzer.polarity_scores(aim)
+            if vs.get('compound') != 0:
+                print(vs.get('compound'))
+                res.append(aim)
+                c_score.append(float(vs.get('compound')))
 
         text = review_text[cur_aspect_end_index_end:]
         get_all(text, aspect, default)
 
     for words in Aspects:
-        get_all(review_text, words, default)
+        get_all(review_text, str(words), default)
         for item in res:
             review_text = review_text.replace(item, "")
 
-    return res
+    if len(c_score) > 0:
+        compound = sum(c_score)/len(c_score)
+        return [res, compound]
 
 
-print(chipotle_reviews[0]['text'])
+concern = 'food'
+'''
+FOOD:
 
 default = ['fast food', 'food chains', 'taco bell', 'making the food', 'order food']
-Aspects = ['food', 'burrito', 'taco', 'chipotle', 'rice', 'bowl', 'chicken', 'beans', 'salsa',
-           'chips', 'meat', 'guacamole', 'steak', 'cheese', 'sour cream', 'lettuce', 'corn',
-           'salad', 'ingredient', 'delicious', 'quality','taste']
+Aspects = ['food', 'bowl', 'burrito', 'taco', 'rice', 'chicken', 'beans', 'salsa', 'chips', 'meat',
+           'steak', 'cheese', 'sour cream', 'lettuce', 'corn', 'salad', 'ingredient', 'delicious', 'taste',
+           'black', 'steak', 'guac', 'sauce', 'fresh', 'veggie', 'cilantro', 'stale', 'cooked', 'salty', 'portion',
+           'amount', 'size','tortilla', 'scoop', 'meal', 'lunch', 'water']
+'''
+'''
+SERVICE:
+'''
+default = ['fast food', 'fast-food', 'am wrong', 'can\'t go wrong', 'I\'m wrong', 'I was wrong']
+Aspects = ['service', 'time', 'attitude', 'staff', 'friendly', 'welcoming', 'manager', 'supervisor',
+            'helpful', 'employee', 'polite', 'fast', 'customer', 'train', 'rude', 'wrong', 
+            'serving', 'line', 'girl', 'guy', 'experience', 'cashier', 'lady','worker', 'minute', 'wait','online',
+            'ready','slow','am','pm', 'pickup','hour','management','gloves', 'hand','app','deliver','missing',
+            'card','phone', 'quick', 'busy', 'counter']#dinnning
 
-tacobell_reviews_food = []
-for r in tacobell_reviews:
-    seg = get_segment(r['text'], Aspects, default)
-    if len(seg) > 0:
-        tacobell_reviews_food.append(seg)
+'''
+PRICE:
+default = ['costume', 'at all cost','accosted']
+Aspects = ['price', 'cheap', 'affordable', 'pricy', 'expensive', 'cost', 'free', 'refund']
+'''
+'''
+AMBIANCE:
+default = ['encountered']
+Aspects = ['ambiance', 'environment', 'atmosphere', 'dirty', 'filthy', 'seat', 'music', 'wifi', 'window',
+            'floor', 'decor', 'door', 'table', 'clean','trash','gloves','disgusting','mess','parking',] #crowd
+'''
 
-chipotle_reviews_food = []
-for r in chipotle_reviews:
-    seg = get_segment(r['text'], Aspects, default)
-    if len(seg) > 0:
-        chipotle_reviews_food.append(seg)
-
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-analyzer = SentimentIntensityAnalyzer()
-
-tacobell_food_score = []
-for sentences in tacobell_reviews_food:
-    for sentence in sentences:
-        vs = analyzer.polarity_scores(sentence)
-        info = [vs.get('neg'), vs.get('pos'), vs.get('neu'), vs.get('compound')]
-        tacobell_food_score.append(info)
-
-chipotle_food_score = []
-for sentences in chipotle_reviews_food:
-    for sentence in sentences:
-        vs = analyzer.polarity_scores(sentence)
-        info = [vs.get('neg'), vs.get('pos'), vs.get('neu'), vs.get('compound')]
-        chipotle_food_score.append(info)
+# text = "I want to be clear in the review. I LOVE chipotles food and i give it 5 stars. \nBut i want to review this location. The most of the staff SUCKS. Especially the manager guy (shaved hair, usually wear a black tshirt). \n\nHe is aggressive toward the customers. One one visit my bf couldn't hear what the girl on the line was asking him so he leaned over a little bit and asked \"what?\" all the sudden the manager guy was staring at him angrily asking if there was a problem. And then continued to stare as we checked out. He is lucky I didn't jump over the counter and ask him if he needed that look taken off his face. To top it off the food was sub par as to what we are used to. \n\nWe didn't visit chipotle for a while and we did again last week. The same guy is still the manager and while he didn't give us any problems, I saw his approach another guest who got lemonade in a water cup. Now I understand you shouldn't be saying you want water and getting a different drink but the manner in which he approached this guy was aggressive. Again the food was bad, my barbacoa tasted like soap and they have lessen the already pitiful amount of chips they give you. Seeing other reviews I see this location needs to GET IT TOGETHER. \n*oh and the parking does suck but i know there isnt much they can do about it."
+# print(get_segment(text, Aspects, default))
 
 
-taco_food = pd.DataFrame(tacobell_food_score)
+find1 = tacobell_reviews
+# tacobell_reviews_sub = [0 for r in find1]
+tacobell_reviews_bid = [r['business_id'] for r in find1]
+tacobell_reviews_date = [r['date'] for r in find1]
+tacobell_reviews_star = [r['stars'] for r in find1]
+tacobell_sep_score = [[None for asp in Aspects] for r in find1]
+for j in range(len(find1)):
+    temp = [None for asp in Aspects]
+    for i in range(len(Aspects)):
+        seg = get_segment(find1[j]['text'], [Aspects[i]], default)
+        if seg:
+            temp[i] = seg[1]
+    tacobell_sep_score[j][:] = temp
 
-chip_food = pd.DataFrame(chipotle_food_score)
 
-taco_food.to_csv("taco_food2.csv", index=False, sep=',')
-chip_food.to_csv("chip_food2.csv", index=False, sep=',')
 
-pd.DataFrame(tacobell_reviews_food).to_csv("taco_reviews_food.csv", index=False, sep=',')
-pd.DataFrame(chipotle_reviews_food).to_csv("chip_reviews_food.csv", index=False, sep=',')
+find2 = chipotle_reviews
+# chipotle_reviews_sub = [0 for r in find2]
+chipotle_reviews_bid = [r['business_id'] for r in find2]
+chipotle_reviews_date = [r['date'] for r in find2]
+chipotle_reviews_star = [r['stars'] for r in find2]
+chipotle_sep_score = [[None for asp in Aspects] for r in find2]
+
+for j in range(len(find2)):
+    temp = [None for asp in Aspects]
+    for i in range(len(Aspects)):
+        seg = get_segment(find2[j]['text'], [Aspects[i]], default)
+        if seg:
+            temp[i] = seg[1]
+    chipotle_sep_score[j][:] = temp
+
+tacobell_sep_score = pd.DataFrame(data=tacobell_sep_score)
+taco_info = pd.DataFrame(data={'business_id': tacobell_reviews_bid, 'date': tacobell_reviews_date,
+                               'stars': tacobell_reviews_star})
+taco_score_chart = pd.concat([taco_info, tacobell_sep_score], axis=1)
+taco_score_chart.columns = ['business_id', 'date', 'stars']+Aspects
+
+
+chipotle_sep_score = pd.DataFrame(data=chipotle_sep_score)
+chip_info = pd.DataFrame(data={'business_id': chipotle_reviews_bid, 'date': chipotle_reviews_date,
+                               'stars': chipotle_reviews_star})
+chip_score_chart = pd.concat([chip_info,chipotle_sep_score], axis=1)
+chip_score_chart.columns = ['business_id', 'date', 'stars']+Aspects
+
+taco_score_chart.to_csv("taco_amb_sep_score.csv", index=False, sep=',')
+chip_score_chart.to_csv("chip_amb_sep_score.csv", index=False, sep=',')
